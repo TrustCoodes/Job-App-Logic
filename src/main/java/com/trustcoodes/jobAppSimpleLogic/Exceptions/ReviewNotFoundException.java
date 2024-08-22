80806b47f6ba629f0d4ab54e60cdff52d2364a87
Trust <bookdjspindim@gmail.com>
2024-08-22 14:08:50 +0100

    MicroService Java
    Java

--- src/main/java/com/trustcoodes/jobAppSimpleLogic/Exceptions/ReviewNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.trustcoodes.jobAppSimpleLogic.Exceptions;
-
-public class ReviewNotFoundException extends RuntimeException{
-    public ReviewNotFoundException(String message){
-        super(message);
-    }
-}
